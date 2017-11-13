cef1d3c170e5e764223a7494af12f882d3280972
winypatrick <winypatric@gmail.com>
2017-11-13 13:35:47 -0100

    ola mundo

--- application/controllers/funcionario.php
+++ application/controllers/funcionario.php
@@ -6,7 +6,7 @@ class Funcionario extends CI_Controller {
   public function __construct()
   {
           parent::__construct();
-          $this->load->model('model_funcionario', 'funcionario');
+          $this->load->model('model_funciona', 'funcionario');
           
   }
 
